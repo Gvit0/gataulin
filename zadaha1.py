de0c4844bf4bef143141aa2697cbb1bdd192d099
user <gatvit@mail.ru>
2023-09-29 16:50:38 +0300

    edit123

--- zadaha1.py
+++ zadaha1.py
@@ -2,18 +2,11 @@ x = int(input())
 y = int(input())
 
 
-if x < 10000 or y < 10000:
-    if x <= y:
-        b = y - x
-        c = x - 1
-        a = c/b
-
-        print(b)
-        print(c)
-        print(a)
-        if isinstance(a, int):
-            print("YES")
-        else:
-            print("NO")
+b = y - x
+print(y,b)
+if y%(b+1)==0:
+    print("YES")
+else:
+    print("NO")
 
 
